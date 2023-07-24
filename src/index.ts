4ac9fa25126ae0674d7d81b98cf026455df9be2e
meowtec <bertonzh@gmail.com>
2023-07-24 10:54:26 +0800

    feat: add analytics

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'prismjs';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import App from './app';
 
+inject();
 createRoot(document.querySelector('#root') as HTMLElement).render(
   React.createElement(App)
 );
